a028d3b22b3518fbeb8a2ac809afc451ed34f1e7
Izunna <cizu64@gmail.com>
2024-08-15 18:16:51 +0100

    Update and rename ConcreteAutomobile.cs to ProfileBuilder.cs

---
+++ DesignPatterns/Builder/ProfileBuilder.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace DesignPatterns.Builder
+{
+    public class ProfileBuilder : IProfileBuilder
+    {
+        Profile profile; //the profile
+        public ProfileBuilder()
+        {
+            profile = new Profile(this);
+        }
+       
+        public Profile Build()
+        {
+            return profile;
+        }
+    
+        public IProfileBuilder SetAge(int age)
+        {
+            profile.Age=age;
+            return this;
+        }
+    
+        public IProfileBuilder SetFullname(string fullname)
+        {
+            profile.Fullname=fullname;
+            return this;
+        }
+    
+        public IProfileBuilder SetGender(string gender)
+        {
+            profile.Gender=gender;
+            return this;    
+        }
+    }
+}
