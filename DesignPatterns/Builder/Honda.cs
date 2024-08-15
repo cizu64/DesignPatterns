2ab3eac6f74b7c3ffa752708484b1a6c363e3f5c
Izunna <cizu64@gmail.com>
2024-08-15 18:15:08 +0100

    Update and rename Honda.cs to Profile.cs

--- DesignPatterns/Builder/Honda.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatterns.Builder
-{
-    public class Honda
-    {
-        public int Id { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-    }
-}
---
+++ DesignPatterns/Builder/Profile.cs
@@ -0,0 +1,13 @@
+﻿namespace DesignPatterns.Builder
+{
+    public class Profile
+    {
+        public Profile(IProfileBuilder profileBuilder)
+        {
+        }
+
+        public string Fullname{get;set;}
+        public string Gender{get;set;}
+        public int Age{get;set;}
+    }
+}
