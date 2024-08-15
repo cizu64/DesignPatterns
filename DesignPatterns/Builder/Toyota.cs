2fa388d23e96cf55ddfea06f7aab56fc7faa38d2
Izunna <cizu64@gmail.com>
2024-08-15 18:14:25 +0100

    Delete DesignPatterns/Builder/Toyota.cs

--- DesignPatterns/Builder/Toyota.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatterns.Builder
-{
-    public  class Toyota
-    {
-        public int Id { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-    }
-}
